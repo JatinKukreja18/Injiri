544629dd304f1f82329a3c8eab9ec8221970fcef
jatin kukreja <jatinkukreja18@yahoo.in>
2018-11-09 17:06:03 +0530

    added collection page styling and functionality

--- wp-content/themes/injiri-theme/template-collections.php
+++ wp-content/themes/injiri-theme/template-collections.php
@@ -1,7 +1,6 @@
 <?php
 /*Template Name: Collections*/
 get_header();
-// this below queries fetches all the countries
 ?>
 <div class=" injiri-wrapper">
 	<div class="row">
@@ -13,7 +12,7 @@ get_header();
     	$term_slug = $term->slug;
     	$_posts = new WP_Query( array(
                 'post_type'         => 'collections',
-                'posts_per_page'    => 10, //important for a PHP memory limit warning
+                'posts_per_page'    => -1, 
                 'tax_query' => array(
                     array(
                         'taxonomy' => 'type',
@@ -24,31 +23,44 @@ get_header();
             ));
 
 	    if( $_posts->have_posts() ) :
-			
+			$count = 0;
+			$count2 = 0;
 		// echo '<h3>'. $term->name .'</h3>';?>
-		<div class="col-lg-4 col-sm-4"></div>
-
-		<div class=" col-lg-5 col-sm-5">
-			<?php while ( $_posts->have_posts() ) : $_posts->the_post();$count++	;
-			?>
-				
-				<li class="collection-thumbnail collection-thumbnail-<?php echo  $term_slug ?> collection-thumbnail-<?php echo  $term_slug ?>-<?php echo  $count ?>" >
-						<?php the_post_thumbnail();?>
-				</li>
-			<?php
-			endwhile;?>
+		<div class="col-lg-8 col-sm-4 offset-4 collection-spacer">
+			<div class="row collection-set">
+				<div class="col-lg-6 col-sm-6 collection-set-image">		
+					<ul>
+					<?php while ( $_posts->have_posts() ) : $_posts->the_post();$count++	;
+					?>
+						
+						<li class="collection-thumbnail collection-thumbnail-<?php echo  $term_slug ?> collection-thumbnail-<?php echo  $term_slug ?>-<?php echo  $count ?> <?php if($count ==1) echo 'active' ?>" >
+								<?php the_post_thumbnail();?>
+						</li>
+					<?php
+					endwhile;?>
+					</ul>
+				</div>
+				<div class=" col-lg-3 col-sm-3 collection-set-list">
+					<?php echo '<h3>'. $term->name .'</h3>';?>
+					<ul class="injiri-list">
+						<?php
+						while ( $_posts->have_posts() ) : $_posts->the_post();$count2++;
+						?>
+							<li class="injiri-list-item collection-link-<?php echo  $term_slug ?> collection-link-<?php echo  $term_slug ?>-<?php echo  $count2 ?> " 
+								>
+								<!-- if on click selected is needed -->
+								<!-- onClick="changeThumbnail('<php echo  $term_slug ?>' , '<php echo  $count2 >')" -->
+								<a  href="<?php the_permalink() ?>"
+								onMouseOver="previewThumbnail(true,'<?php echo  $term_slug ?>' , '<?php echo  $count2 ?>')"
+								onMouseLeave="previewThumbnail(false,'<?php echo  $term_slug ?>' , '<?php echo  $count2 ?>')"
+								
+								class="injiri-list-link" ><?php the_title(); ?></a>
+							</li>
+						<?php endwhile;?>
+					</ul>
+				</div>
+			</div>
 		</div>
-		<div class=" col-lg-3 col-sm-3">
-
-		<?php echo '<h3>'. $term->name .'</h3>';?>
-		<?php
-        while ( $_posts->have_posts() ) : $_posts->the_post();$count2++;
-        ?>
-				<li class="stockist-place" onMouseOver="changeThumbnail('<?php echo  $term_slug ?>' , '<?php echo  $count2 ?>')">
-					<span class="stockist-title" ><?php the_title(); ?></span>
-				</li>
-			<?php
-			endwhile;?></div>
         <?php
         
 
@@ -60,16 +72,28 @@ endforeach;
 	
 </div>
 <script>
+	function previewThumbnail(action,type,index){
+		console.log(action)
+		if(action) 	document.querySelector(`.collection-thumbnail-${type}-` + index).classList.add("preview")
+		else	document.querySelector(`.collection-thumbnail-${type}-` + index).classList.remove("preview")	
+	}
 	function changeThumbnail(type,index){
 		removeAllThumbnail(type);
+		removeActiveLinks(type);
 		document.querySelector(`.collection-thumbnail-${type}-` + index).classList.add("active")
+		document.querySelector(`.collection-link-${type}-` + index).classList.add("active")
 	}
 	function removeAllThumbnail(type){
 		let allImages = document.querySelectorAll(".collection-thumbnail-" + type);
 		for(let i=0; i < allImages.length ; i++){
 			allImages[i].classList.remove('active')
 		}
-		
+	}
+	function removeActiveLinks(type){
+		let allLinks = document.querySelectorAll(".collection-link-" + type);
+		for(let i=0; i < allLinks.length ; i++){
+			allLinks[i].classList.remove('active')
+		}
 	}
 </script>
 </div>
